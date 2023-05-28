d13af4a099d88dae01f7ea40efe853591949591a
lukabalaban <luka.balaban@student.um.si>
2023-05-29 01:14:51 +0200

    Install react-native-paper on project

--- client/RoadAnalyzerApp/screens/SensorScreen.js
+++ client/RoadAnalyzerApp/screens/SensorScreen.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from "react";
+import { Card, Button } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
 import {
   Platform,
   StyleSheet,
