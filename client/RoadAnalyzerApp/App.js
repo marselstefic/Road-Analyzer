1420b39f112792dd0df1a43a6c8603f96361038b
marselstefic <marsel.stefic@gmail.com>
2023-05-30 19:31:25 +0200

    Partial implementation of HomeScreen component

--- client/RoadAnalyzerApp/App.js
+++ client/RoadAnalyzerApp/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import SensorScreen from './screens/SensorScreen'; // Import SensorScreen component
+import SensorScreen from './screens/SensorScreen'; 
 import RegisterScreen from './screens/RegisterScreen';
+import HomeScreen from './screens/RegisterScreen';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Register">
-      <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Navigator initialRouteName="HomeScreen">
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="SensorScreen" component={SensorScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- client/RoadAnalyzerApp/screens/HomeScreen.js
+++ client/RoadAnalyzerApp/screens/HomeScreen.js
@@ -6,13 +6,39 @@ import RegisterScreen from './screens/RegisterScreen';
 
 const Stack = createStackNavigator();
 
-export default function App() {
+export default function RegisterScreen({ navigation }) {
+  const ToLoginScreen = async () => {
+    navigation.navigate('LoginScreen');
+  }
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Register">
-      <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="SensorScreen" component={SensorScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Button
+        title="Register"
+        onPress={ToLoginScreen}
+      />
+      <Button
+        title="Login"
+        onPress={ToLoginScreen}
+      />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 16,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 10,
+    padding: 8,
+  },
+});
